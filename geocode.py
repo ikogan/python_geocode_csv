f7c62ae78eb6eaf6a1391a28dd108ac81128a3c8
Ilya Kogan <kogan@ohio.edu>
2014-09-21 18:45:20 -0400

    Lots of improvements to the original.
    - Respect daily API call limit for public use.
    - Allow specifying a "batch" number which will skip
      all records prior to that batch.
    - Remove Panda dependency as it's overkill for just reading
      CSVs.
    - Require specifying a "key column" to retain in the output.
    - Since CSVReader is a stream reader, we don't need to load
      the whole CSV into memory for reading.

--- geocode.py
+++ geocode.py
@@ -7,37 +7,60 @@
 from geopy.geocoders import GoogleV3
 geolocator = GoogleV3()
 
+API_CALL_LIMIT = 2000
+
 import sys
+
+if len(sys.argv) < 3:
+	print >> sys.stderr, "Usage:", sys.argv[0], " <input file> <key column>"
+	print >> sys.stderr, """    Note that the input file should not contain the ".csv" extension.
+    If your filename is "sample_data.csv" then the first argument should
+    be "sample_data". The key column is a column in your input that must be
+    preserved in the output file."""
+	sys.exit(1)
 
 file_name=sys.argv[1]
 
-input_file = "%s.csv" % (file_name)
-output_file = "%s.coded.csv" % (file_name)
+input_file = open("%s.csv" % (file_name), 'r')
+output_file = open("%s.coded.csv" % (file_name), 'w')
+key_column = sys.argv[2]
 
-import pandas as pd
-df = pd.read_csv(input_file)
+if len(sys.argv) > 3:
+	batch = int(sys.argv[3])
+else:
+	batch = 1
 
-# clean data
-df=df.fillna('')
-df=df.rename(columns=lambda x: x.strip())
+import csv
+incsv = csv.DictReader(input_file, dialect="excel")
+outcsv = csv.writer(output_file)
 
-df['latitude']=0.0
-df['longitude']=0.0
+incsv.fieldnames = [x.strip() for x in incsv.fieldnames]
 
-for i,row in df.iterrows():
-    address = "%s, %s, %s, %s" % (row['Address'], row['City'], row['State'],row['Zip'])
+if not key_column in incsv.fieldnames:
+	print >> sys.stderr, "[ERR] Cannot find key column %s in input data." % (key_column)
+	sys.exit(2)
+
+outcsv.writerow([key_column, 'Address', 'Longitude', 'Latitude'])
+
+if batch > 1:
+	print "Seeking to start of batch %d at record %d..." % (batch, (batch*API_CALL_LIMIT)-1)
+	for i in range(1,(batch*API_CALL_LIMIT)-1):
+		incsv.next()
+
+api_count = 0
+for row in incsv:
+    address = "%s, %s, %s, %s" % (row['Address'], row['City'], row['State'], row['Zip'])
 
     try:
         address, (latitude, longitude) = geolocator.geocode(address)
+        api_count += 1
     except:
         print "[ERR] FAILED TO GET LOCATION FOR ", address
     else:
-        df['latitude'][i] = latitude
-        df['longitude'][i] = longitude
         print address, longitude, latitude
+        outcsv.writerow([row[key_column], address, longitude, latitude])
 
-
-# write geocoded data to file
-print "saving to " + output_file
-df.to_csv(output_file)
+    if api_count >= API_CALL_LIMIT:
+    	print "Limit of %d calls hit, stopping." % (API_CALL_LIMIT)
+    	break
 
